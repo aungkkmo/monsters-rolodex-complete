2576a5c5a2f524c93ec0a634b93f1a811f8405d3
Aung Ko Ko Myint Oo <aungkoko.m.o@seasoft.asia>
2019-10-13 20:53:07 +0630

    Updates

---
+++ precache-manifest.8df3328e72dd9c72abbd32f4e2f7accb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0fad99df2525e6a4838a11c4414737",
+    "url": "/monsters-rolodex-complete/index.html"
+  },
+  {
+    "revision": "6f8468a27e6a00ff6ede",
+    "url": "/monsters-rolodex-complete/static/css/main.91c2703e.chunk.css"
+  },
+  {
+    "revision": "0cca02fe10f4d25a3546",
+    "url": "/monsters-rolodex-complete/static/js/2.e74b291d.chunk.js"
+  },
+  {
+    "revision": "6f8468a27e6a00ff6ede",
+    "url": "/monsters-rolodex-complete/static/js/main.62b08cf7.chunk.js"
+  },
+  {
+    "revision": "854e8690a751bdaaf55b",
+    "url": "/monsters-rolodex-complete/static/js/runtime-main.d3534e38.js"
+  }
+]);
